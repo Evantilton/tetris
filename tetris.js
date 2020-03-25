b834721af8cf3b3eeac99a4dc93e386e3ee65b56
evantilton <evantilton@gmail.com>
2020-03-24 23:58:04 -0500

    made it proper size and started on player functionality and control

--- tetris.js
+++ tetris.js
@@ -1,26 +1,37 @@
 const canvas = document.getElementById('tetris');
 
-const context = canvas.getContext('2d')
+const context = canvas.getContext('2d');
 
+context.scale(20, 20);
 //sets the color to black, 000 is black
-context.fillstyle = '#000'
+context.fillstyle = '#000';
 
-context.fillRect(0,0, canvas.width, canvas.height);
+context.fillRect(0, 0, canvas.width, canvas.height);
 
 //making the 2d matrix for the T piece
 const matrix = [
-    [0,0,0],
-    [1,1,1],
-    [0,1,0],
+    [0, 0, 0],
+    [1, 1, 1],
+    [0, 1, 0],
 ]
 
-//because we want each 0 to be nothing, if it's not zero (1) we set it to red
-matrix.forEach((row, y) => {
-    row.forEach((value, x) => {
-        if (value !== 0) {
-            context.fillStyle = 'red'
-            //left, top, width, height
-            context.fillRect(x, y, 1, 1):
-        }
+function drawMatrix(matrix, offset) {
+    //because we want each 0 to be nothing, if it's not zero (1) we set it to red
+    matrix.forEach((row, y) => {
+        row.forEach((value, x) => {
+            if (value !== 0) {
+                context.fillStyle = 'red';
+                //left, top, width, height
+                context.fillRect(x + offset.x,
+                                 y + offset.y,
+                                  1, 1);
+            }
+        });
     });
-});
+};
+
+const player = {
+    pos: {x: 5, y: 5},
+    matrix: matrix,
+}
+drawMatrix(player.matrix, player.pos);
