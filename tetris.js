398536bff66cb4de10107eef5777a0d9bef30911
evantilton <evantilton@gmail.com>
2020-03-28 13:14:30 -0500

    got the board to redraw properly when the piece is moved so it doesn't overlap

--- tetris.js
+++ tetris.js
@@ -1,13 +1,7 @@
 const canvas = document.getElementById('tetris');
-
 const context = canvas.getContext('2d');
 
 context.scale(20, 20);
-    //sets the color to black, 000 is black
-    context.fillstyle = '#000';
-    context.fillRect(0, 0, canvas.width, canvas.height);
-
-
 
 //making the 2d matrix for the T piece
 const matrix = [
@@ -17,20 +11,20 @@ const matrix = [
 ]
 
 function draw() {
-    //sets the color to black, 000 is black
-    context.fillstyle = '#000';
+    //this draws the board, 000 is code for black
+    context.fillStyle = '#000';
     context.fillRect(0, 0, canvas.width, canvas.height);
 
     drawMatrix(player.matrix, player.pos);
 }
 
 function drawMatrix(matrix, offset) {
-    //because we want each 0 to be nothing, if it's not zero (1) we set it to red
+    //this checks the board and because we want 0 to be blank, 
+    //we check if it's not zero and color that piece
     matrix.forEach((row, y) => {
         row.forEach((value, x) => {
             if (value !== 0) {
                 context.fillStyle = 'red';
-                //left, top, width, height
                 context.fillRect(x + offset.x,
                                  y + offset.y,
                                  1, 1);
@@ -38,22 +32,22 @@ function drawMatrix(matrix, offset) {
         });
     });
 }
-let dropCounter = 0;
-//this is milliseconds, so every 1 second
-//it drops the piece every 1 second
-let dropInterval = 1000;
-let lastTime = 0;
+// let dropCounter = 0;
+// //this is milliseconds, so every 1 second
+// //it drops the piece every 1 second
+// let dropInterval = 1000;
+// let lastTime = 0;
 //this keeps it continually updating on every move
 function update(time = 0) {
-    const deltaTime = time - lastTime;
-    lastTime = time;
+    // const deltaTime = time - lastTime;
+    // lastTime = time;
 
-    dropCounter += deltaTime;
-    if (dropCounter > dropInterval) {
-        player.pos.y++;
-        dropCounter = 0;
-    }
-    console.log(deltaTime);
+    // dropCounter += deltaTime;
+    // if (dropCounter > dropInterval) {
+    //     player.pos.y++;
+    //     dropCounter = 0;
+    // }
+    // console.log(deltaTime);
     draw();
     requestAnimationFrame(update);
 }
