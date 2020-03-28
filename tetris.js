9cc62156df58312a849ce1162d6bf5283e9e8756
evantilton <evantilton@gmail.com>
2020-03-28 16:22:59 -0500

    added scoring and scoreboard, fully functional now

--- tetris.js
+++ tetris.js
@@ -3,7 +3,22 @@ const context = canvas.getContext('2d');
 
 context.scale(20, 20);
 
+function arenaSweep() { 
+    let rowCount = 1;
+    outer: for (let y = arena.length -1; y > 0; --y) {
+        for (let x = 0; x < arena[y].length; ++x) {
+            if (arena[y][x] === 0) {
+                continue outer;
+            }
+        }
 
+        const row = arena.splice(y, 1)[0].fill(0);
+        arena.unshift(row);
+        ++y;
+        player.score += rowCount * 10;
+        rowCount *= 2;
+    }
+}
 
 function collide(arena, player) {
     const [m,o] = [player.matrix, player.pos];
@@ -34,8 +49,59 @@ function createPiece(type) {
             [1, 1, 1],
             [0, 1, 0],
         ];
+    } else if (type === 'O') {
+        return [
+            [2, 2],
+            [2, 2],
+        ];
+    } else if (type === 'L') {
+        return [
+            [0, 3, 0],
+            [0, 3, 0],
+            [0, 3, 3],
+        ];
+    } else if (type === 'J') {
+        return [
+            [0, 4, 0],
+            [0, 4, 0],
+            [4, 4, 0],
+        ];
+    }  else if (type === 'I') {
+        return [
+            [0, 5, 0, 0],
+            [0, 5, 0, 0],
+            [0, 5, 0, 0],
+            [0, 5, 0, 0],
+        ];
+    }  else if (type === 'S') {
+        return [
+            [0, 6, 6],
+            [6, 6, 0],
+            [0, 0, 0],
+        ];
+    } else if (type === 'Z') {
+        return [
+            [7, 7, 0],
+            [0, 7, 7],
+            [0, 0, 0],
+        ];
+    } 
+}
+
+function playerReset() {
+     const pieces = 'TJLOSZI';
+     player.matrix = createPiece(pieces[pieces.length * Math.random() | 0])
+    player.pos.y = 0;
+    player.pos.x = (arena[0].length / 2 | 0) -
+                    (player.matrix[0].length / 2 | 0);
+    if (collide(arena,player)) {
+        arena.forEach(row => row.fill(0));
+        player.score = 0;
+        updateScore();
     }
 }
+
+
 function draw() {
     //this draws the board, 000 is code for black
     context.fillStyle = '#000';
@@ -50,7 +116,7 @@ function drawMatrix(matrix, offset) {
     matrix.forEach((row, y) => {
         row.forEach((value, x) => {
             if (value !== 0) {
-                context.fillStyle = 'red';
+                context.fillStyle = colors[value],
                 context.fillRect(x + offset.x,
                                  y + offset.y,
                                  1, 1);
@@ -74,7 +140,9 @@ function playerDrop() {
     if (collide(arena, player)) {
         player.pos.y--;
         merge(arena, player);
-        player.pos.y = 0;
+        playerReset();
+        arenaSweep();
+        updateScore();
     }
     dropCounter = 0;
 }
@@ -138,13 +206,27 @@ function update(time = 0) {
     }
     draw();
     requestAnimationFrame(update);
-}
+} 
+const colors = [
+    null,
+    'red',
+    'blue',
+    'violet',
+    'green',
+    'purple',
+    'orange',
+    'pink',
+]
 const arena = createMatrix(12, 20);
 
-//sets the position of the piece the player controls 
 const player = {
-    pos: {x: 5,y: 5},
-    matrix: createPiece('T'),
+    pos: {x: 0, y: 0},
+    matrix: null,
+    score: 0,
+};
+
+function updateScore() {
+    document.getElementById('score').innerText = player.score;
 }
 
 document.addEventListener('keydown', event => {
@@ -161,4 +243,6 @@ document.addEventListener('keydown', event => {
         playerRotate(1);
     }
 });
+playerReset();
+updateScore();
 update();
