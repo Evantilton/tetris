f6256fd41614ca391f65eee3ff49f340c0108fa7
evantilton <evantilton@gmail.com>
2020-03-28 14:58:18 -0500

    added the rotate and wall rotate collision detection

--- tetris.js
+++ tetris.js
@@ -74,6 +74,50 @@ function playerDrop() {
     }
     dropCounter = 0;
 }
+
+//this controls the movement of each direction
+// and keeps it so it won't exit the arena
+function playerMove(dir) {
+    player.pos.x += dir;
+    if (collide(arena, player)) {
+        player.pos.x -= dir;
+    }
+}
+function playerRotate(dir) {
+    const pos = player.pos.x;
+    let offset = 1;
+    rotate(player.matrix, dir);
+    while (collide(arena, player)) {
+        player.pos.x += offset;
+        offset = -(offset + (offset > 0 ? 1 : -1));
+        if (offset > player.matrix[0].length) {
+            rotate(player.matrix, -dir);
+            player.pos.x = pos;
+            return;
+        }
+
+    }
+}
+
+//creating the rotation of the grid
+function rotate(matrix, dir) {
+    for (let y = 0; y < matrix.length; ++y) {
+        for (let x = 0; x < y; ++x) {
+            [
+                matrix[x][y],
+                matrix[y][x],
+            ] = [
+                matrix[y][x],
+                matrix[x][y],
+            ];
+        }
+    }
+    if (dir > 0) {
+        matrix.forEach(row => row.reverse());
+    } else {
+        matrix.reverse();
+    }
+}
 let dropCounter = 0;
 //this is milliseconds, so every 1 second
 //it drops the piece every 1 second
@@ -102,11 +146,15 @@ const player = {
 document.addEventListener('keydown', event => {
     console.log(event);
     if (event.keyCode === 37) {
-        player.pos.x--;
+        playerMove(-1);
     } else if (event.keyCode === 39) {
-        player.pos.x++;
+        playerMove(1);
     } else if (event.keyCode === 40 ) {
-        playerDrop();
+        playerDrop(); 
+    } else if (event.keyCode === 81) {
+        playerRotate(-1);
+    } else if (event.keyCode === 87) {
+        playerRotate(1);
     }
 });
 update();
